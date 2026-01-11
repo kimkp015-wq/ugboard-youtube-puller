35c7f365369ca054e9e8a647f9b9dd7264e8d364
kimkp015-wq <kimkp015@gmail.com>
2026-01-11 04:48:45 +0300

    Update worker.ts

--- src/worker.ts
+++ src/worker.ts
@@ -1,3 +1,8 @@
+export interface Env {
+  ENGINE_BASE_URL: string
+  INTERNAL_TOKEN: string
+}
+
 export default {
   async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext) {
     const res = await fetch(`${env.ENGINE_BASE_URL}/ingest/youtube`, {
